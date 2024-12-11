eeed3dca5c17f0116b973a05cff2d0bf6f18e083
Zena_Hoseok <vera.and.nadezda@mail.ru>
2024-12-11 23:22:54 +0300

    боковая навигация работает

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/example/digitallibrary/databinding/ActivityMenuOfflineBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/example/digitallibrary/databinding/ActivityMenuOfflineBinding.java
@@ -4,37 +4,48 @@ package com.example.digitallibrary.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.fragment.app.FragmentContainerView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewbinding.ViewBinding;
 import androidx.viewbinding.ViewBindings;
 import com.example.digitallibrary.R;
+import com.google.android.material.navigation.NavigationView;
 import java.lang.NullPointerException;
 import java.lang.Override;
 import java.lang.String;
 
 public final class ActivityMenuOfflineBinding implements ViewBinding {
   @NonNull
-  private final ConstraintLayout rootView;
+  private final DrawerLayout rootView;
 
   @NonNull
-  public final FragmentContainerView fragmentContainerView;
+  public final DrawerLayout drawerLayout;
 
   @NonNull
-  public final ConstraintLayout main;
+  public final FrameLayout fragmentContainer;
 
-  private ActivityMenuOfflineBinding(@NonNull ConstraintLayout rootView,
-      @NonNull FragmentContainerView fragmentContainerView, @NonNull ConstraintLayout main) {
+  @NonNull
+  public final NavigationView navView;
+
+  @NonNull
+  public final Toolbar toolbar;
+
+  private ActivityMenuOfflineBinding(@NonNull DrawerLayout rootView,
+      @NonNull DrawerLayout drawerLayout, @NonNull FrameLayout fragmentContainer,
+      @NonNull NavigationView navView, @NonNull Toolbar toolbar) {
     this.rootView = rootView;
-    this.fragmentContainerView = fragmentContainerView;
-    this.main = main;
+    this.drawerLayout = drawerLayout;
+    this.fragmentContainer = fragmentContainer;
+    this.navView = navView;
+    this.toolbar = toolbar;
   }
 
   @Override
   @NonNull
-  public ConstraintLayout getRoot() {
+  public DrawerLayout getRoot() {
     return rootView;
   }
 
@@ -59,16 +70,28 @@ public final class ActivityMenuOfflineBinding implements ViewBinding {
     // This is done to optimize the compiled bytecode for size and performance.
     int id;
     missingId: {
-      id = R.id.fragmentContainerView;
-      FragmentContainerView fragmentContainerView = ViewBindings.findChildViewById(rootView, id);
-      if (fragmentContainerView == null) {
+      DrawerLayout drawerLayout = (DrawerLayout) rootView;
+
+      id = R.id.fragment_container;
+      FrameLayout fragmentContainer = ViewBindings.findChildViewById(rootView, id);
+      if (fragmentContainer == null) {
         break missingId;
       }
 
-      ConstraintLayout main = (ConstraintLayout) rootView;
+      id = R.id.nav_view;
+      NavigationView navView = ViewBindings.findChildViewById(rootView, id);
+      if (navView == null) {
+        break missingId;
+      }
 
-      return new ActivityMenuOfflineBinding((ConstraintLayout) rootView, fragmentContainerView,
-          main);
+      id = R.id.toolbar;
+      Toolbar toolbar = ViewBindings.findChildViewById(rootView, id);
+      if (toolbar == null) {
+        break missingId;
+      }
+
+      return new ActivityMenuOfflineBinding((DrawerLayout) rootView, drawerLayout,
+          fragmentContainer, navView, toolbar);
     }
     String missingId = rootView.getResources().getResourceName(id);
     throw new NullPointerException("Missing required view with ID: ".concat(missingId));
--- app/src/main/java/com/example/digitallibrary/offline/MenuOfflineActivity.kt
+++ app/src/main/java/com/example/digitallibrary/offline/MenuOfflineActivity.kt
@@ -1,21 +1,71 @@
 package com.example.digitallibrary.offline
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.digitallibrary.R
+import com.example.digitallibrary.fragment.AboutAppFragment
+import com.example.digitallibrary.fragment.ArchivFragment
+import com.example.digitallibrary.fragment.BookmarkFragment
+import com.example.digitallibrary.fragment.ChoiceFragment
+import com.example.digitallibrary.fragment.ImportWorkFragment
+import com.example.digitallibrary.fragment.LikeFragment
+import com.example.digitallibrary.fragment.NewWorkFragment
+import com.example.digitallibrary.fragment.ProfilFragment
+import com.example.digitallibrary.fragment.SettingFragment
+import com.example.digitallibrary.register.VhodFragment
+import com.google.android.material.navigation.NavigationView
 
-class MenuOfflineActivity : AppCompatActivity() {
+class MenuOfflineActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_menu_offline)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = null//отключение заголовка у toolbar
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, MainMenuOfflineFragment()).commit()
+            navigationView.setCheckedItem(R.id.nav_main_menu_offline)
         }
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_main_menu_offline -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, MainMenuOfflineFragment()).commit()
+            R.id.nav_settings -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, SettingFragment()).commit()
+            R.id.nav_about -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, AboutAppFragment()).commit()
+            R.id.nav_archiv -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, ArchivFragment()).commit()
+            R.id.nav_bookmark -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, BookmarkFragment()).commit()
+            R.id.nav_choice -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, ChoiceFragment()).commit()
+            R.id.nav_import -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, ImportWorkFragment()).commit()
+            R.id.nav_new_work -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, NewWorkFragment()).commit()
+            R.id.nav_profil -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, ProfilFragment()).commit()
+            R.id.nav_like -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, LikeFragment()).commit()
+            R.id.nav_logout ->  supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, VhodFragment()).commit()
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
